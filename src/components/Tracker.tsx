a47588f030bae491df7dcc5605c0aa0969cbe04b
maria-jon <mariajonasson93@gmail.com>
2026-01-05 15:16:10 +0100

    :art: remove unnecessary code

--- src/components/Tracker.tsx
+++ src/components/Tracker.tsx
@@ -118,83 +118,13 @@ function reducer(state: TrackerState, action: Action): TrackerState {
 }
 
 export default function Tracker() {
-
   const [state, dispatch] = useReducer(reducer, initialState);
-  /*
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [activeIndex, setActiveIndex] = useState<number>(0);
-  const [activeId, setActiveId] = useState<string | null>(null);
-
-  const [round, setRound] = useState<number>(1);
-  */
-  //const addRound = () => setRound((i) => i + 1);
-
+  
   function toNumberOrEmpty(value: string) {
     if (value.trim() === "") return "";
     const n = Number(value);
     return Number.isNaN(n) ? "" : n;
   }
-  /*
-  function updateCharacter(id: string, patch: Partial<Character>) {
-    setCharacters(prev => prev.map(c => (c.id === id ? { ...c, ...patch } : c)));
-  }
-
-  function addNew() {
-    setCharacters(prev => [
-      ...prev,
-      {
-        id: crypto.randomUUID(),
-        name: "",
-        initiative: "",
-        hp: "",
-        ac: "",
-        type: "pc",
-        hasActed: false,
-      },
-    ]);
-  }
-
-  function sortByInitiative() {
-    setCharacters(prev => {
-      const sorted = [...prev].sort((a, b) => (Number(b.initiative) || 0) - (Number(a.initiative) || 0));
-      return sorted.map(c => ({ ...c, hasActed: false }));
-    });
-    setActiveIndex(0);
-  }
-
-  function nextTurn() {
-    setCharacters(prev => {
-      if (prev.length === 0) return prev;
-  
-      const currentIndex = activeId ? prev.findIndex(c => c.id === activeId) : 0;
-      const safeIndex = currentIndex === -1 ? 0 : currentIndex;
-  
-      const current = prev[safeIndex];
-      const updated = prev.map(c => (c.id === current.id ? { ...c, hasActed: true } : c));
-  
-      console.log(`Current:`, current);
-      console.log(`Characters:`, characters);
-
-      const nextIndex = (safeIndex + 1) % prev.length;
-      setActiveId(prev[nextIndex].id);
-  
-      return updated;
-    });
-
-    console.log(`Characters:`, characters);
-    
-    const allHasActed = (character: Character) => {
-      return character.hasActed === true;
-    }
-    if (characters.every(allHasActed)) {
-      addRound();
-      setCharacters(character => {
-        const resetHasActed = character.map(c => ({ ...c, hasActed: false}));
-        return resetHasActed;
-      })
-    }
-  }
-  */
 
   return (
   <div className="tracker-wrapper">
