75c6ab42a450cae2cc5272eec2957bd74c6b4cff
maria-jon <mariajonasson93@gmail.com>
2026-01-16 13:20:35 +0100

    :wheelchair: add aria-label to HealthInput button

--- src/components/tracker/HealthInput.tsx
+++ src/components/tracker/HealthInput.tsx
@@ -98,6 +98,7 @@ export default function HealthInput({
         className={`button-icon plus-minus 
           ${hasActed ? "" : "button-dark"}`
         }
+        aria-label="Toggle menu to adjust health"
       >
         <PlusMinusIcon 
             size={24}
