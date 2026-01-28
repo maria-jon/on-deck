1b9af0d0ba7a4971de9b9e8f3265d80b2bc654bf
maria-jon <mariajonasson93@gmail.com>
2026-01-28 17:47:23 +0100

    :bento: add reset icon

--- src/components/utils/IconSVGs.tsx
+++ src/components/utils/IconSVGs.tsx
@@ -169,7 +169,6 @@ export const HeartMinusIcon = (props: IconProps) => {
   )
 }
 
-
 export const LinkIcon = (props: IconProps) => {
   return(
     <svg 
@@ -180,6 +179,22 @@ export const LinkIcon = (props: IconProps) => {
       fill={props.fill}
       aria-hidden={props.ariaHidden}
     >
-      <path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/>    </svg>
+      <path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/>    
+    </svg>
+  )
+}
+
+export const ResetIcon = (props: IconProps) => {
+  return(
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      height={props.size}
+      viewBox="0 -960 960 960" 
+      width={props.size}
+      fill={props.fill}
+      aria-hidden={props.ariaHidden}
+    >
+      <path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/>    
+    </svg>
   )
 }
