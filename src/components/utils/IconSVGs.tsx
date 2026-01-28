3f6493dd0b61c1b037b8f7bfee636b4426ce777d
maria-jon <mariajonasson93@gmail.com>
2026-01-28 19:08:58 +0100

    :bento: add menu and close icons

--- src/components/utils/IconSVGs.tsx
+++ src/components/utils/IconSVGs.tsx
@@ -198,3 +198,33 @@ export const ResetIcon = (props: IconProps) => {
     </svg>
   )
 }
+
+export const MeunIcon = (props: IconProps) => {
+  return(
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      height={props.size}
+      viewBox="0 -960 960 960" 
+      width={props.size}
+      fill={props.fill}
+      aria-hidden={props.ariaHidden}
+    >
+      <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>    
+    </svg>
+  )
+}
+
+export const CloseIcon = (props: IconProps) => {
+  return(
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      height={props.size}
+      viewBox="0 -960 960 960" 
+      width={props.size}
+      fill={props.fill}
+      aria-hidden={props.ariaHidden}
+    >
+      <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>    
+    </svg>
+  )
+}
